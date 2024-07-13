cde78fe397f97ed736183a85c66df88143e5f43d
zom <contact.zomzd@gmail.com>
2024-07-13 19:06:49 +0200

    Map correctly displaying

--- src/render/render.c
+++ src/render/render.c
@@ -6,7 +6,7 @@
 /*   By: truello <truello@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 15:18:18 by truello           #+#    #+#             */
-/*   Updated: 2024/07/11 17:27:42 by truello          ###   ########.fr       */
+/*   Updated: 2024/07/13 19:06:16 by truello          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static void	render_minimap(t_image *image)
 		while (image->global->map[i][j] != -2)
 		{
 			if (image->global->map[i][j] == 1)
-				draw_square(image, make_square(i * 17, j * 17, 16, rgb(0, 0, 0)));
+				draw_square(image, make_square(j * 33, i * 33, 32, rgb(255, 255, 255)));
 			j++;
 		}
 		i++;
@@ -45,9 +45,9 @@ int	render_cub3d(t_global *global)
 	main_image.addr = mlx_get_data_addr(main_image.img,
 			&(main_image.bits_per_pixel), &(main_image.line_length),
 			&(main_image.endian));
-	mlx_put_image_to_window(global->mlx, global->mlx_win, main_image.img, 0, 0);
-	render_minimap(&main_image);
 	render_raycast(&main_image);
+	render_minimap(&main_image);
+	mlx_put_image_to_window(global->mlx, global->mlx_win, main_image.img, 0, 0);
 	mlx_destroy_image(global->mlx, main_image.img);
 	return (0);
 }
