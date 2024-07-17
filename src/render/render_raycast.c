3dff670eb8a8dbbb8770ad899e5ecd6202a0b733
zom <contact.zomzd@gmail.com>
2024-07-17 14:04:49 +0200

    Norm

--- src/render/render_raycast.c
+++ src/render/render_raycast.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render_raycast.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tohma <tohma@student.42.fr>                +#+  +:+       +#+        */
+/*   By: truello <truello@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 11:07:55 by truello           #+#    #+#             */
-/*   Updated: 2024/07/17 00:14:15 by tohma            ###   ########.fr       */
+/*   Updated: 2024/07/17 14:04:39 by truello          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,22 @@ void	calculate_ray_dist(t_ray *ray, t_player *player, float cameraX)
 	calculate_side_dist(ray, player);
 }
 
+static void	check_side(t_ray *ray, int axis)
+{
+	if (axis == 0)
+	{
+		if (ray->ray_dir_x > 0.0f)
+			ray->side = 2;
+		else
+			ray->side = 3;
+		return ;
+	}
+	if (ray->ray_dir_y > 0.0f)
+		ray->side = 0;
+	else
+		ray->side = 1;
+}
+
 /* Orientations
 	0 : N
 	1 : S
@@ -72,13 +88,13 @@ void	check_hit_walls(t_ray *ray, t_global *global, t_player *player)
 		{
 			ray->side_dist_x += ray->delta_dist_x;
 			map_x += ray->step_x;
-			ray->side = ray->ray_dir_x > 0.0f ? 2 : 3;
+			check_side(ray, 0);
 		}
 		else
 		{
 			ray->side_dist_y += ray->delta_dist_y;
 			map_y += ray->step_y;
-			ray->side = ray->ray_dir_y > 0.0f ? 0 : 1;
+			check_side(ray, 1);
 		}
 		if (global->map[map_y][map_x] == MAP_WALL
 			|| global->map[map_y][map_x] == MAP_CLOSED_DOOR)
